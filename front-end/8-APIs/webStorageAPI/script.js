be417763a0a4b12076d02bab442c609f74a01c06
Marina de Moraes <mrndemoraes@gmail.com>
2026-07-10 15:52:27 -0300

    dando erro na mudança de tema

--- front-end/8-APIs/webStorageAPI/script.js
+++ front-end/8-APIs/webStorageAPI/script.js
@@ -1,3 +1,17 @@
+const btnTema = document.getElementById("btn-tema");
+
+btnTema.addEventListener("click", () => {
+  //verificando tema
+  const temaAtual = localStorage.getItem("tema");
+  //verificando que tema e invertendo
+  const novoTema = temaAtual === "escuro" ? "claro" : "escuro";
+  //adicionando a classe "escuro" ao elemento body
+  document.body.classList.toggle(novoTema);
+  //Salvar a preferência do usuário no LocalStorage
+  localStorage.setItem("tema", novoTema);
+  btnTema.textContent = novoTema === "escuro" ? "☀︎" : "☾";
+});
+
 // principais métodos
 /*localStorage.setItem("chave", valor);
 localStorage.getItem("chave");
