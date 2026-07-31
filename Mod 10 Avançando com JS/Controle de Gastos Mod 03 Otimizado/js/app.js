fd6ae81ef9b5f9b39fad404cfd2206fe289e3df0
Marina de Moraes <mrndemoraes@gmail.com>
2026-07-31 16:50:54 -0300

    transformando codigo em POO

--- Mod 10 Avançando com JS/Controle de Gastos Mod 03 Otimizado/js/app.js
+++ Mod 10 Avançando com JS/Controle de Gastos Mod 03 Otimizado/js/app.js
@@ -1,71 +1,31 @@
-const matrizGastos = [
-  ["Alimentação", 0],
-  ["Transporte", 0],
-  ["Lazer", 0],
-  ["Outros", 0],
-  ["Total", 0],
-];
+import { Categoria, ListaGastosPorCategoria } from "./classes.js";
+import { valorNegativo, atualizandoInterface } from "./utils.js";
+// POO
+const gastosPorCategoria = new ListaGastosPorCategoria(
+  new Categoria("Alimentação"),
+  new Categoria("Transporte"),
+  new Categoria("Lazer"),
+  new Categoria("Outros"),
+);
 
-/* ------------------------- funções utilitárias ------------------------- */
-const obterElemento = (id) => document.getElementById(id); //obter elemento
-const valorNegativo = (valor) => valor < 0;
-const somaValor = (total, valor) => total + valor;
-const limpandoCampos = () => (obterElemento("valor").value = "");
-const formatandoMoeda = (valor) => valor.toFixed(2).replace(".", ",");
+// Manipulando o DOM
+const formulario = document.querySelector("form");
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-/* --------------------- obtendo valores do usuário --------------------- */
-// obtendo valores do form
-const obterValorInformado = () => parseFloat(obterElemento("valor").value); //obter valor
-const obterCategoriaInformada = () => obterElemento("categoria").value;
-
-// obtendo categoria da matriz
-const obterCategoria = (matriz, nomeCategoria) =>
-  matriz.find((item) => item[0] === nomeCategoria);
-
-// atualizando valores da matriz
-const atualizandoValorCategoria = (categoria, valor) =>
-  (categoria[1] = somaValor(categoria[1], valor));
-
-/* ----------------------- atualizando interface ----------------------- */
-const atualizandoInterface = () => {
-  matrizGastos.forEach(([nome, valor]) => {
-    const elemento = obterElemento(nome);
-    elemento.textContent = `${nome}: R$ ${formatandoMoeda(valor)}`;
-  });
-};
-
-/* ----------------------- FUNÇÃO PRINCIPAL ----------------------- */
-function adicionarGasto() {
-  const valorInformado = obterValorInformado();
-  const categoriaInformada = obterCategoriaInformada();
+  // Acessando .elements através da prop name do html
+  const valorInformado = formulario.elements.valor.value;
+  const categoriaInformada = formulario.elements.categoria.value;
 
   if (valorNegativo(valorInformado)) {
     alert("Valor inválido. O valor não pode ser negativo.");
     return;
   }
 
-  const categoria = obterCategoria(matrizGastos, categoriaInformada);
-  const total = obterCategoria(matrizGastos, "Total");
+  const categoria =
+    gastosPorCategoria.obterCategoriaPorNome(categoriaInformada);
+  categoria.adicionarValor(valorInformado);
 
-  atualizandoValorCategoria(categoria, valorInformado);
-  atualizandoValorCategoria(total, valorInformado);
-  atualizandoInterface();
-  limpandoCampos();
-}
-/* --------------------------------------------------------------------- */
-/* Montando o código
-1. Crie a função, aplique ao botão e o teste com um console.log (adicione "teste" à função)
-
-2. o que ter na função
-function adicionarGasto() {
-  1.pegar valor informado
-  2. pegar categoria informada
-  3. impedir numeros negativos
-  4. ao clicar em adicionar, atualizar valor de acordo com categoria
-   4.1 criar variaveis para armazenar os valores de cada categoria
-  5. atualizar interface
-  6. limpar campos
-}
-
-
-*/
+  atualizandoInterface(gastosPorCategoria);
+  formulario.reset();
+});
---
+++ Mod 10 Avançando com JS/Controle de Gastos Mod 03 Otimizado/js/utils.js
@@ -0,0 +1,18 @@
+// Funções utilitárias
+const obterElemento = (id) => document.getElementById(id); //obter elemento
+const formatandoMoeda = (valor) => valor.toFixed(2).replace(".", ",");
+
+export const valorNegativo = (valor) => valor < 0;
+
+// Atualizando Interface
+export const atualizandoInterface = (gastosPorCategoria) => {
+  const categorias = gastosPorCategoria.categorias;
+
+  categorias.forEach(({ nome, valor }) => {
+    const elemento = obterElemento(nome);
+    elemento.textContent = `${nome}: R$ ${formatandoMoeda(valor)}`;
+  });
+
+  const elementoTotal = obterElemento("Total");
+  elementoTotal.textContent = `Total: R$ ${formatandoMoeda(gastosPorCategoria.obterTotal())}`;
+};
